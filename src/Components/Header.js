e6a1dbdb0c85dc5c116e8f8079c4d867ea91f7d7
con0fav <connor.favero@gmail.com>
2021-03-26 23:47:48 -0700

    search bar component created, added to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
+import SearchBar from "./SearchBar"
 
 function Header() {
   return (
     <nav class="navbar navbar-dark bg-dark">
       <div class="container-fluid">
         <h1 class="navbar-brand mb-0">Employee Directory</h1>
+        <form class="d-flex">
+          <SearchBar />
+        </form>
       </div>
     </nav>
   );
