08f39ca4da2ae1ea417378a806ff0c1422f30e98
con0fav <connor.favero@gmail.com>
2021-03-26 23:34:32 -0700

    basic nav/header added

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container-fluid">
+        <h1 class="navbar-brand mb-0">Employee Directory</h1>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
