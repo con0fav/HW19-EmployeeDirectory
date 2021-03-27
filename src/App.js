848b4945f5bea4950249b5ee6bc4f0b8a179397f
con0fav <connor.favero@gmail.com>
2021-03-27 10:29:13 -0700

    added dummy table, api function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./Components/Header"
+import Table from "./Components/Table"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Table />
     </div>
   );
 }
