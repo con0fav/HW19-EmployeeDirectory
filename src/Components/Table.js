7180ffc13be3909931277748c0a9d91e0ea1f71c
con0fav <connor.favero@gmail.com>
2021-03-29 15:28:03 -0700

    trying different render

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,25 +1,19 @@
 import TableBody from "./TableBody";
 
-function Table({headings, emp, handleSort}) {
+function Table({ headings, emp, handleSort }) {
     return (
         <table class="table">
             <thead>
                 <tr>
-                    {headings.map(({name, width}) => {
-                        return (
-                            <th 
-                            scope="col"
-                            key={name}
-                            style={{width}}
-                            onClick={() => {
-                                handleSort(name.toLowerCase());
-                            }}>{name}</th>
-
-                        )
-                    })}
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">City</th>
+                    <th scope="col">Email</th>
                 </tr>
             </thead>
-            <TableBody emp={emp} />
+            <tbody>
+                <TableBody />
+            </tbody>
         </table>
     )
 }
