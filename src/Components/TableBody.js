7035850c7c044ac6d67b504b53adeb8793d94108
con0fav <connor.favero@gmail.com>
2021-03-29 01:12:37 -0700

    structure for table body

--- src/Components/TableBody.js
+++ src/Components/TableBody.js
@@ -1,22 +1,41 @@
-return (
-    <tbody>
-        {emp[0] !== undefined && emp[0].name !== undefined ? (
-            emp.map(({login, picture, name, city, email }) => {
-                return (
-                    <tr key={login.uuid}>
-                        <td data-th="Image" className="align-middle">
-                            <img 
-                            src={picture.medium}
-                            alt={"image for" + name.first}
-                            className="img-responsive"
-                            />
-                        </td>
-                        <td data-th="name" className="name-cell align-middle">
-                            {name.first} {name.last}
-                        </td>
-                    </tr>
-                )
-            })
-        )}
-    </tbody>
-)
+function TableBody({emp}) {
+
+    return (
+        <tbody>
+            {emp[0] !== undefined && emp[0].name !== undefined ? (
+                emp.map(({ login, picture, name, city, email }) => {
+                    return (
+                        <tr key={login.uuid}>
+                            <td data-th="Image" className="align-middle">
+                                <img
+                                    src={picture.medium}
+                                    alt={"image for" + name.first}
+                                    className="img-responsive"
+                                />
+                            </td>
+                            <td data-th="name" className="name-cell align-middle">
+                                {name.first} {name.last}
+                            </td>
+                            <td>
+                                <td data-th="city" className="name-cell align-middle">
+                                    {location.city}
+                                </td>
+                            </td>
+                            <td>
+                                <td data-th="email" className="name-cell align-middle">
+                                    {email}
+                                </td>
+                            </td>
+                        </tr>
+                    )
+                })
+            ) : (
+                <></>
+            )}
+        </tbody>
+    )
+
+}
+
+
+export default TableBody;
