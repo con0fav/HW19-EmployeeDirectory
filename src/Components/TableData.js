3eea506e73221cc73f1d3a2f4359e94ad7d50c1f
con0fav <connor.favero@gmail.com>
2021-03-29 01:43:22 -0700

    testing population

--- src/Components/TableData.js
+++ src/Components/TableData.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Table from "./Table";
 import API from "../utils/API";
 
@@ -9,10 +9,38 @@ export default class TableData extends Component {
         filteredEmp: [{}]
     }
 
-    
+    headings = [
+        { name: "Image", width: "10%" },
+        { name: "Name", width: "10%" },
+        { name: "City", width: "20%" },
+        { name: "Email", width: "20%" },
+    ]
 
 
+    //space for additional features
 
 
+    componentDidMount() {
+        API.getEmp().then(res => {
+            this.setState({
+                emp: res.data.res,
+                filteredEmp: res.data.res
+            })
+        })
+    }
+
+
+    render() {
+        return (
+            <>
+                <div className = "data-area">
+                    <Table
+                    headings={this.headings}
+                    emp={this.state.filteredEmp} 
+                    />
+                </div>
+            </>
+        )
+    }
 
 }
